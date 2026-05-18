cf7bdf582cd3973a1518087c1159deec31602dae
test <yourgithubemail@example.com>
2026-05-18 11:40:27 +0530

    new changes

--- test.js
+++ test.js
@@ -1,20 +1,26 @@
-const { Builder } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
+const { Builder, By } = require("selenium-webdriver");
 
-(async function example() {
-    let options = new chrome.Options();
-
-    options.addArguments("--headless");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-
+(async function test() {
     let driver = await new Builder()
         .forBrowser("chrome")
-        .setChromeOptions(options)
         .build();
 
-    await driver.get("https://www.google.com");
-    console.log("Test Passed");
+    try {
+        await driver.get("https://plaxonic.com");
+
+        await driver.sleep(5000);
+
+        await driver.findElement(
+            By.xpath("//a[text()='What we do']")
+        ).click();
+
+        console.log("Clicked successfully");
+
+        await driver.sleep(5000);
+
+    } catch(err){
+        console.log(err);
+    }
 
     await driver.quit();
 })();
