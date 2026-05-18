6408d00e02f40b41936633087eb56eee42b287d3
test <yourgithubemail@example.com>
2026-05-18 11:43:46 +0530

    new changes

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
 const { Builder, By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 
 (async function test() {
+    let options = new chrome.Options();
+
+    options.addArguments("--headless");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+
     let driver = await new Builder()
         .forBrowser("chrome")
+        .setChromeOptions(options)
         .build();
 
     try {
@@ -16,7 +24,7 @@ const { Builder, By } = require("selenium-webdriver");
 
         console.log("Clicked successfully");
 
-        await driver.sleep(5000);
+        await driver.sleep(3000);
 
     } catch(err){
         console.log(err);
