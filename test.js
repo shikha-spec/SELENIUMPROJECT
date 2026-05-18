9576c23d7bbccc51e812d2ca074bae833bc0f729
test <yourgithubemail@example.com>
2026-05-18 18:26:56 +0530

    New changes

--- test.js
+++ test.js
@@ -1,34 +1,20 @@
-const { Builder, By } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
-
+const{ Builder, By, WebDriver } = require("selenium-webdriver");
 (async function test() {
-    let options = new chrome.Options();
+let driver = await new Builder()
+.forBrowser("chrome")
+.build();
+await driver.get("https://plaxonic.com");
+await driver.sleep(5000);
+await driver.findElement(By.xpath("//a[contains(@class,'h-full flex flex-col justify-center')][normalize-space()='What we do']")).click();
+await driver.sleep(6000);
+await driver.findElement(By.xpath("//a[normalize-space()='Web Apps Development']")).click();
+await driver.sleep(5000);
+await driver.navigate().back();
+await driver.sleep(5000);
+await driver.findElement(By.xpath("//a[contains(@class,'h-full flex flex-col justify-center')][normalize-space()='What we do']")).click();
+await driver.sleep(6000);
+await driver.findElement(By.xpath("//p[normalize-space()='Design Consulting']")).click();
+await driver.sleep(4000);
+await driver.quit();
 
-    options.addArguments("--headless");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-
-    let driver = await new Builder()
-        .forBrowser("chrome")
-        .setChromeOptions(options)
-        .build();
-
-    try {
-        await driver.get("https://plaxonic.com");
-
-        await driver.sleep(5000);
-
-        await driver.findElement(
-            By.xpath("//a[text()='What we do']")
-        ).click();
-
-        console.log("Clicked successfully");
-
-        await driver.sleep(3000);
-
-    } catch(err){
-        console.log(err);
-    }
-
-    await driver.quit();
 })();
